6f8018c31102c84d3b09265d0b068704b013c01b
barantoraman <barantoraman@protonmail.com>
2025-08-21 18:16:19 +0300

    feat: add chain of responsibility

---
+++ cmd/behavioral/cor/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/barantoraman/DesignPatternsTraining/internal/behavioral/cor"
+)
+
+func main() {
+	cashier := &cor.Cashier{}
+
+	medical := &cor.Medical{}
+	medical.SetNext(cashier)
+
+	doc := &cor.Doctor{}
+	doc.SetNext(medical)
+
+	reception := &cor.Reception{}
+	reception.SetNext(doc)
+
+	patient := &cor.Patient{Name: "abc"}
+	reception.Execute(patient)
+}
